5ba4776f991ceed95179b2c763a7c38479502155
Mr. Natural <76610619+itsmrnatural@users.noreply.github.com>
2023-03-15 12:50:17 +0530

    fix: description alignment

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
             <div className="bg-neutral-800/10 shadow-xl rounded-lg w-full h-auto mt-6">
                 <div className="relative">
                     <div className="flex flex-col lg:flex-row justify-between w-full p-6 px-8 items-center h-full">
-                        <div className="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full">
+                        <div className="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full lg:w-4/5">
                             <div className="flex items-center">
                                 <p className="flex items-center text-white text-4xl font-semibold">
                                     Hey, I am Mr. Natural!👋
