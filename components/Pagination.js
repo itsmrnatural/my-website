0a4e990ec003eae407beec06c62bb916ac8ade4f
Mr. Natural <76610619+itsmrnatural@users.noreply.github.com>
2023-12-03 06:57:22 +0000

    refactor(blog): BlogCard and Pagination components

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,15 +1,20 @@
+import React from 'react';
+
 const Pagination = ({
     currentPage,
     totalPages,
     handlePreviousPage,
     handleNextPage,
 }) => {
+    const isFirstPage = currentPage === 1;
+    const isLastPage = currentPage === totalPages;
+
     return (
         <div className="flex justify-between items-center w-full mt-5">
             <button
-                className="bg-white/5 hover:bg-white/10 border border-transparent hover:border-white text-white text-sm px-3 py-2 rounded-lg"
+                className={`bg-white/5 hover:bg-white/10 border border-transparent hover:border-white text-white text-sm px-3 py-2 rounded-lg ${isFirstPage ? 'cursor-not-allowed' : 'cursor-pointer'}`}
                 onClick={handlePreviousPage}
-                disabled={currentPage === 1}
+                disabled={isFirstPage}
             >
                 ← Back
             </button>
@@ -17,9 +22,9 @@ const Pagination = ({
                 Page {currentPage}/{totalPages}
             </p>
             <button
-                className="bg-white/5 hover:bg-white/10 border border-transparent hover:border-white text-white text-sm px-3 py-2 rounded-lg"
+                className={`bg-white/5 hover:bg-white/10 border border-transparent hover:border-white text-white text-sm px-3 py-2 rounded-lg ${isLastPage ? 'cursor-not-allowed' : 'cursor-pointer'}`}
                 onClick={handleNextPage}
-                disabled={currentPage === totalPages}
+                disabled={isLastPage}
             >
                 Next →
             </button>
