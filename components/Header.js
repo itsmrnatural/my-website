06b65e09d1c7f3ad705f54ed49d2b5b9f4b7fd7b
Natural <76610619+itsmrnatural@users.noreply.github.com>
2025-03-06 07:46:40 +0000

    fix: :zap: improve loading time btw page transitions
    Previously preset 1000ms loading delay was hampering performance, this commit fixes the issue and also introduces preloading of the other pages to improve transtion experience.
    
    This fixes the issue #10 hopefully.

--- components/Header.js
+++ components/Header.js
@@ -1,82 +1,75 @@
-const items = [
-    { icon: "fa fa-chess-pawn", link: "https://chess.com/member/itsmrnatural" },
-    { icon: "fab fa-twitter", link: "https://twitter.com/itsmrnatural" },
-    {
-        icon: "fab fa-discord",
-        link: "https://discord.com/users/624572769484668938",
-    },
-    { icon: "fab fa-github", link: "https://github.com/itsmrnatural" },
-];
-
-const navItems = [
-    { icon: "fal fa-home", active: "fa fa-home", label: "Home", href: "/" },
-    {
-        icon: "fal fa-folder",
-        active: "fa fa-folder",
-        label: "Projects",
-        href: "/projects",
-    },
-    {
-        icon: "fal fa-user",
-        active: "fa fa-user",
-        label: "About Me",
-        href: "/aboutme",
-    },
-    {
-        icon: "fal fa-th-list",
-        active: "fa fa-th-list",
-        label: "Blog",
-        href: "/blog",
-    },
-];
-
 import Link from "next/link";
 import { useRouter } from "next/router";
 
+const items = [
+  { icon: "fab fa-twitter", link: "https://twitter.com/itsmrnatural" },
+  {
+    icon: "fab fa-discord",
+    link: "https://discord.com/users/624572769484668938",
+  },
+  { icon: "fab fa-github", link: "https://github.com/itsmrnatural" },
+];
+
+const navItems = [
+  { icon: "fal fa-home", active: "fa fa-home", label: "Home", href: "/" },
+  {
+    icon: "fal fa-folder",
+    active: "fa fa-folder",
+    label: "Projects",
+    href: "/projects",
+  },
+  {
+    icon: "fal fa-user",
+    active: "fa fa-user",
+    label: "About Me",
+    href: "/aboutme",
+  },
+  {
+    icon: "fal fa-th-list",
+    active: "fa fa-th-list",
+    label: "Blog",
+    href: "/blog",
+  },
+];
+
 const Header = () => {
-    const router = useRouter();
-    return (
-        <nav className="w-full border-b-2 border-neutral-800/20 pb-2">
-            <div className="flex flex-col md:flex-row w-full items-center md:justify-between">
-                <p className="font-semibold text-xl">Mr. Natural</p>
-                <div className="flex items-center space-x-2">
-                    {items.map((item) => (
-                        <a
-                            key={item.link}
-                            href={item.link}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="flex items-center justify-center hover:bg-neutral-700/20 rounded-xl transition-all duration-150 p-2 px-3"
-                        >
-                            <i
-                                className={`${item.icon} text-2xl hover:text-white`}
-                            />
-                        </a>
-                    ))}
-                </div>
-            </div>
-            <div className="flex justify-center md:justify-start items-center space-x-4 py-2">
-                {navItems.map((item) => (
-                    <Link key={item.label} href={item.href}>
-                        <a
-                            className={`flex items-center justify-center text-white/50 cursor-pointer hover:text-white/100 rounded-xl transition-all duration-150 ${
-                                router.asPath === item.href && "text-white-100"
-                            }`}
-                        >
-                            <i
-                                className={`${
-                                    router.asPath === item.href
-                                        ? item.active
-                                        : item.icon
-                                } mr-2`}
-                            />
-                            {item.label}
-                        </a>
-                    </Link>
-                ))}
-            </div>
-        </nav>
-    );
+  const router = useRouter();
+  const isActive = (href) => router.asPath === href;
+
+  return (
+    <nav className="w-full border-b-2 border-neutral-800/20 pb-2">
+      <div className="flex flex-col md:flex-row w-full items-center md:justify-between">
+        <p className="font-semibold text-xl">Mr. Natural</p>
+        <div className="flex items-center space-x-2">
+          {items.map(({ link, icon }) => (
+            <a
+              key={link}
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center hover:bg-neutral-700/20 rounded-xl transition-all duration-150 p-2 px-3"
+            >
+              <i className={`${icon} text-2xl hover:text-white`} />
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-center md:justify-start items-center space-x-4 py-2">
+        {navItems.map(({ label, href, icon, active }) => (
+          <Link key={label} href={href}>
+            <a
+              className={`flex items-center justify-center text-white/50 cursor-pointer hover:text-white/100 rounded-xl transition-all duration-150 ${
+                isActive(href) && "text-white-100"
+              }`}
+            >
+              <i className={`${isActive(href) ? active : icon} mr-2`} />
+              {label}
+            </a>
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,86 +9,138 @@ import "tippy.js/animations/shift-toward.css";
 import Router, { useRouter } from "next/router";
 import { Fragment, useEffect, useState } from "react";
 import { Transition } from "@headlessui/react";
+
 const Header = dynamic(() => import("../components/Header"));
 const Footer = dynamic(() => import("../components/Footer"));
 
+// Required for smoother page transitions
+const preloadPages = () => {
+  const importPage = (path) => {
+    switch (path) {
+      case "/":
+        import("../pages/index");
+        break;
+      case "/aboutme":
+        import("../pages/aboutme");
+        break;
+      case "/blog":
+        import("../pages/blog");
+        break;
+      case "/projects":
+        import("../pages/projects");
+        break;
+      default:
+        break;
+    }
+  };
+
+  const navItems = ["/", "/aboutme", "/blog", "/projects"];
+  navItems.forEach((item) => importPage(item));
+};
+
 function MyApp({ Component, pageProps }) {
-    let [load, setLoad] = useState(false);
-    useEffect(() => {
-        setTimeout(() => {
-            setLoad(true);
-            document.documentElement.style = "pointer-events: all;";
-        }, 1000);
-    }, []);
-    Router.events.on("routeChangeStart", () => {
-        setLoad(false);
-        document.documentElement.style = "pointer-events: none;";
-    });
-    Router.events.on("routeChangeComplete", () => {
-        setTimeout(() => {
-            setLoad(true);
-            document.documentElement.style = "pointer-events: all;";
-        }, 1000);
-    });
-    Router.events.on("routeChangeError", () => {
-        setTimeout(() => {
-            setLoad(true);
-            document.documentElement.style = "pointer-events: all;";
-        }, 1000);
-    });
-    return (
-        <>
-            <Head>
-                <title>Mr. Natural • Home</title>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
-                <link
-                    href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css"
-                    rel="stylesheet"
-                />
-            </Head>
-            <Transition
-                as={Fragment}
-                show={!load ? true : false}
-                enter="transform duration-[50ms] transition"
-                enterFrom="opacity-0"
-                enterTo="opacity-80"
-                leave="transform duration-[100ms] transition ease-in-out"
-                leaveFrom="opacity-80"
-                leaveTo="opacity-0"
-            >
-                <div
-                    style={{ zIndex: 99999 }}
-                    className="fixed bg-black/75 w-full h-screen flex justify-center items-center pointer-events-none"
-                >
-                    <div className="flex items-center gap-x-6 animate-pulse">
-                        <div className="text-center">
-                            <p className="text-6xl mb-5 font-semibold">
-                                Loading...
-                            </p>
-                            <p className="uppercase text-xl font-semibold text-white">
-                                <i className="fal fa-spinner-third fa-spin" />
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </Transition>
-            <main className="border-b-[7px] border-t-[7px] h-full border-neutral-800/50 w-full">
-                <div className="min-h-screen max-w-screen-lg p-5 w-full md:w-10/12 lg:w-8/12 mx-auto transition-all duration-300">
-                    <Header />
-                    <Component {...pageProps} />
-                </div>
-                <div className=" p-4 w-full md:w-10/12 lg:w-8/12 mx-auto transition-all duration-300">
-                    <div className="bg-neutral-800/5">
-                        <div className="flex flex-row mx-20">
-                            <Footer />
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </>
-    );
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    // Intial page load
+    const intialLoadTimer = setTimeout(() => {
+      setIsLoading(false);
+      document.documentElement.style = "pointer-events: all;";
+    }, 300);
+
+    // Route change start
+    const handleStart = () => {
+      setIsLoading(true);
+      document.documentElement.style = "pointer-events: none;";
+    };
+
+    // Route change complete
+    const handleComplete = () => {
+      const completeTimer = setTimeout(() => {
+        setIsLoading(false);
+        document.documentElement.style = "pointer-events: all;";
+      }, 150);
+
+      return () => clearTimeout(completeTimer);
+    };
+
+    // Route change error
+    const handleError = () => {
+      setIsLoading(false);
+      document.documentElement.style = "pointer-events: all;";
+    };
+
+    // Add event listeners
+    Router.events.on("routeChangeStart", handleStart);
+    Router.events.on("routeChangeComplete", handleComplete);
+    Router.events.on("routeChangeError", handleError);
+
+    // Clean up event listeners on unmount
+    return () => {
+      clearTimeout(intialLoadTimer);
+      Router.events.off("routeChangeStart", handleStart);
+      Router.events.off("routeChangeComplete", handleComplete);
+      Router.events.off("routeChangeError", handleError);
+    };
+  }, []);
+
+  // Prefetching pages
+  useEffect(() => {
+    const prefetchPages = () => {
+      router.prefetch("/");
+      router.prefetch("/aboutme");
+      router.prefetch("/projects");
+      router.prefetch("/blog");
+    };
+    prefetchPages();
+  }, [router]);
+
+  return (
+    <>
+      <Head>
+        <title>Mr. Natural • Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet" />
+      </Head>
+      <Transition
+        as={Fragment}
+        show={isLoading}
+        enter="transform duration-[50ms] transition"
+        enterFrom="opacity-0"
+        enterTo="opacity-80"
+        leave="transform duration-[100ms] transition ease-in-out"
+        leaveFrom="opacity-80"
+        leaveTo="opacity-0"
+      >
+        <div
+          style={{ zIndex: 99999 }}
+          className="fixed bg-black/75 w-full h-screen flex justify-center items-center pointer-events-none"
+        >
+          <div className="flex items-center gap-x-6 animate-pulse">
+            <div className="text-center">
+              <p className="text-6xl mb-5 font-semibold">Loading...</p>
+              <p className="uppercase text-xl font-semibold text-white">
+                <i className="fal fa-spinner-third fa-spin" />
+              </p>
+            </div>
+          </div>
+        </div>
+      </Transition>
+      <main className="border-b-[7px] border-t-[7px] h-full border-neutral-800/50 w-full">
+        <div className="min-h-screen max-w-screen-lg p-5 w-full md:w-10/12 lg:w-8/12 mx-auto transition-all duration-300">
+          <Header />
+          <Component {...pageProps} />
+        </div>
+        <div className=" p-4 w-full md:w-10/12 lg:w-8/12 mx-auto transition-all duration-300">
+          <div className="bg-neutral-800/5">
+            <div className="flex flex-row mx-20">
+              <Footer />
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 export default MyApp;
