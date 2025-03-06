ec3ff9f4213077407f1e76a50aa284273d8ff241
Natural <76610619+itsmrnatural@users.noreply.github.com>
2025-03-06 10:23:38 +0000

    fix(eslint): :bug: disable react/no-unescaped-entities rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['next/core-web-vitals'],
   rules: {
+    'react/no-unescaped-entities': 'off',
     // Fix passHref warnings
     '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-no-target-blank': 'off',
