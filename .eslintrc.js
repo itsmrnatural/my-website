5ec6d369c8016087b6b791a0c1c960d4723d2dc5
Natural <76610619+itsmrnatural@users.noreply.github.com>
2025-03-06 10:16:20 +0000

    chore: :sparkles: migrate ESLint configuration from .eslintrc.json to .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['next/core-web-vitals'],
+  rules: {
+    // Fix passHref warnings
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-no-target-blank': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+  },
+};
