ba841b5ca1379f5a82f2a1f444a484d0ff755de5
Natural <76610619+itsmrnatural@users.noreply.github.com>
2025-11-30 18:14:41 +0000

    fix: remove viewport meta from _document.jsx
    Next.js warns against placing viewport meta in _document.
    It's already defined in _app.jsx.

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,10 +13,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
